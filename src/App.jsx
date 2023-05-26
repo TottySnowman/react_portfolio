23386cf1e0a3694656aee24dc9b349bfdbe22fee
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2023-05-26 22:07:58 +0200

    Added Yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import React from 'react'
-import Header from './components/header/header'
-import Nav from './components/nav/Nav'
-import About from './components/about/about'
-import Experience from './components/experience/experience'
-import Portfolio from './components/portfolio/portfolio'
-import Footer from './components/footer/footer'
-import Contact from './components/contact/contact'
-import About_new from './components/about_new/about_new'
-import {useEffect} from 'react'
-
-
+import React from "react";
+import Header from "./components/header/header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/about";
+import Experience from "./components/experience/experience";
+import Portfolio from "./components/portfolio/portfolio";
+import Footer from "./components/footer/footer";
+import Contact from "./components/contact/contact";
+import About_New from "./components/about_new/about_new";
+import { useEffect } from "react";
 
 const App = () => {
-  useEffect(() =>{
+  useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) =>{
-          if(entry.isIntersecting){
-              entry.target.classList.add('show');
-          }
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
       });
+    });
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
   });
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((el) => observer.observe(el));
-  })
   return (
     <>
-      <Header/>
-      <Nav/>
-      <About/>
-      <Experience/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
-      <About_new/>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+      <About_New />
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
