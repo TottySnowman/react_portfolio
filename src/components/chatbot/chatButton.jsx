dcadad4eda0f3e06b18f7afb9636c9f86f662abf
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2025-02-22 22:01:56 +0100

    added websocket connection and handling and spinner

--- src/components/chatbot/chatButton.jsx
+++ src/components/chatbot/chatButton.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import "./chatbot.css";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const ChatButton = () => {
   const [isChatOpen, setIsChatOpen] = useState(false);
+  const [socket, setSocket] = useState(null);
+  const [modelState, setModelState] = useState(null);
   const [messages, setMessages] = useState([
     { text: "Hello! How can I assist you today?", role: "system" },
   ]);
@@ -28,11 +31,33 @@ const ChatButton = () => {
     }
   };
 
+  const handleChatOpenClick = async () => {
+    if (socket == null) {
+      const ws = new WebSocket("ws:localhost:6001/chat/ws");
+
+      ws.onopen = () => {
+        ws.send("get model state");
+      };
+
+      ws.onmessage = (event) => {
+        setModelState(event.data);
+      };
+
+      ws.onclose = () => {
+        setSocket(null);
+      };
+
+      setSocket(ws);
+    }
+
+    setIsChatOpen(!isChatOpen);
+  };
+
   return (
     <div className="chat-container">
       <button
         className="chat-button"
-        onClick={() => setIsChatOpen(!isChatOpen)}
+        onClick={async () => await handleChatOpenClick()}
       >
         {isChatOpen ? "Close Chat" : "Start Chat"}
       </button>
@@ -49,27 +74,32 @@ const ChatButton = () => {
             </button>
           </div>
 
-          <div className="chat-body" ref={chatBodyRef}>
-            {messages.map((msg, index) => (
-              <div key={index} className={`chat-message ${msg.role}`}>
-                {msg.text}
+          {modelState === "STARTING" && <ClipLoader />}
+          {modelState === "RUNNING" && (
+            <>
+              <div className="chat-body" ref={chatBodyRef}>
+                {messages.map((msg, index) => (
+                  <div key={index} className={`chat-message ${msg.role}`}>
+                    {msg.text}
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
 
-          <div className="chat-footer">
-            <input
-              type="text"
-              placeholder="Type a message..."
-              className="chat-input"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
-            />
-            <button className="send-button" onClick={handleSendMessage}>
-              Send
-            </button>
-          </div>
+              <div className="chat-footer">
+                <input
+                  type="text"
+                  placeholder="Type a message..."
+                  className="chat-input"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
+                />
+                <button className="send-button" onClick={handleSendMessage}>
+                  Send
+                </button>
+              </div>
+            </>
+          )}
         </div>
       )}
     </div>
